3e5119cf79ab75810ae509c36489eb29bc5d0d42
aimen <artem.igorevich8@rambler.ru>
2023-02-25 20:41:48 +0300

    add main page of our app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const booksRouter = require('./routes/books');
 const userRouter = require('./routes/user');
@@ -7,9 +8,10 @@ const indexRouter = require('./routes');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.use(express.json());
-app.use('/api', indexRouter);
+app.use('/', indexRouter);
 app.use('/api/books', booksRouter);
 app.use('/api/user', userRouter);
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+let { store } = require('../store');
+
 router.get('/', (req, res) => {
-  const { url } = req;
-  res.json({ url });
+  res.render('index', {
+    title: 'Список книг',
+    books: store.books,
+  });
 });
 
 module.exports = router;
