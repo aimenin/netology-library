8fb3f7be0aff2525d97e6fa41b473964edd4cf12
aimen <artem.igorevich8@rambler.ru>
2023-03-26 18:29:49 +0300

    add new ejs components WIP

--- library/src/routes/api/user.js
+++ library/src/routes/api/user.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/login', (req, res) => {
+  res.render('user/login', {
+    title: 'Login',
+  });
+});
+
+router.get('/me', (req, res) => {
+  res.render('user/profile', {
+    title: 'Profile',
+  });
+});
+
 router.post('/login', (req, res) => {
   res.status(201);
   res.json({ id: 1, mail: 'test@mail.ru' });
