e7ff6a34a0e1f95cc6e94a28a0f8513b409336db
aimen <artem.igorevich8@rambler.ru>
2023-03-27 21:38:39 +0300

    add routes for authentication WIP

--- library/src/routes/api/user.js
+++ library/src/routes/api/user.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const passport = require('passport');
+
+const User = require('../../store/schema/User');
+
 const router = express.Router();
 
 router.get('/login', (req, res) => {
@@ -11,6 +15,45 @@ router.get('/me', (req, res) => {
   res.render('user/profile', {
     title: 'Profile',
   });
+});
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    failureRedirect: '/login',
+    failureFlash: true,
+  }),
+  function (req, res) {
+    res.redirect('/');
+  }
+);
+
+router.post('/signup', (req, res) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: 'Your account could not be saved. Error: ' + err,
+        });
+      } else {
+        req.login(user, (er) => {
+          if (er) {
+            res.json({ success: false, message: er });
+          } else {
+            res.json({ success: true, message: 'Your account has been saved' });
+          }
+        });
+      }
+    }
+  );
+});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/login');
 });
 
 router.post('/login', (req, res) => {
