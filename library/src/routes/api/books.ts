2a27ce2440fc600914eb664c574465f5af5bc0eb
aimen <artem.igorevich8@rambler.ru>
2023-05-22 20:16:34 +0300

    migrate api folder to typescript

--- library/src/routes/api/books.ts
+++ library/src/routes/api/books.ts
@@ -1,12 +1,136 @@
-import express from 'express';
+// import express from 'express';
 
-import { container } from '../../types/Container';
-import { BooksRepository } from '../../types/Book';
+// import { container } from '../../types/Container';
+// import { BooksRepository } from '../../types/Book';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get(':id', async (req, res, next) => {
-  const repo = container.get(BooksRepository);
-  const book = await repo.getBook(req.params.id);
-  res.json(book);
+// router.get(':id', async (req, res, next) => {
+//   const repo = container.get(BooksRepository);
+//   const book = await repo.getBook(req.params.id);
+//   res.json(book);
+// });
+
+import { Router } from 'express';
+import appRoot from 'app-root-path';
+const router = Router();
+
+import Book from '../../store/schema/Book';
+import fileMulter from '../../middlewares/file';
+
+router.get('/', async (req, res) => {
+  try {
+    const books = await Book.find().select('-__v');
+    res.json(books);
+  } catch (e) {
+    res.status(500).json(e);
+  }
 });
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const book = await Book.findById(id).select('-__v');
+
+    if (!book) {
+      res.status(404);
+      res.json('404 | книга не найдена');
+      return;
+    }
+
+    res.json(book);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+router.post('/', async (req, res) => {
+  const {
+    title,
+    description,
+    authors,
+    favorite,
+    fileCover,
+    fileName,
+    fileBook,
+  } = req.body;
+
+  const newBook = new Book({
+    title,
+    description,
+    authors,
+    favorite,
+    fileCover,
+    fileName,
+    fileBook,
+  });
+
+  try {
+    await newBook.save();
+    res.json(newBook);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const {
+    title,
+    description,
+    authors,
+    favorite,
+    fileCover,
+    fileName,
+    fileBook,
+  } = req.body;
+
+  try {
+    const book = await Book.findByIdAndUpdate(id, {
+      title,
+      description,
+      authors,
+      favorite,
+      fileCover,
+      fileName,
+      fileBook,
+    });
+    res.status(200).json(book);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Book.deleteOne({ _id: id });
+    res.json(true);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+router.post('/upload-file', fileMulter.single('book-file'), (req, res) => {
+  if (req.file) {
+    const { filename } = req.file;
+    res.json({ filename });
+  }
+  res.json();
+});
+
+router.get('/:id/download', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const book = await Book.findById(id).select('-__v');
+    const file = `${appRoot}/public/books/${book.fileBook}`;
+    res.download(file);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+export default router;
---
+++ library/src/routes/api/user.ts
@@ -0,0 +1,75 @@
+import { Router } from 'express';
+import passport from 'passport';
+
+const User = require('../../store/schema/User');
+
+const router = Router();
+
+router.get('/login', (req, res) => {
+  res.render('user/login', {
+    title: 'Login',
+  });
+});
+
+router.get(
+  '/me',
+  (req, res, next) => {
+    if (!req.isAuthenticated()) {
+      return res.redirect('/api/user/login');
+    }
+    next();
+  },
+  (req, res) => {
+    res.render('user/profile', {
+      title: 'Profile',
+      user: {
+        name: req.user.username,
+      },
+    });
+  }
+);
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    failureRedirect: '/api/user/login',
+    failureFlash: true,
+  }),
+  function (req, res) {
+    res.status(200).json({ message: 'ok' });
+  }
+);
+
+router.post('/signup', (req, res) => {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: 'Your account could not be saved. Error: ' + err,
+        });
+      } else {
+        req.login(user, (er) => {
+          if (er) {
+            res.json({ success: false, message: er });
+          } else {
+            res.json({ success: true, message: 'Your account has been saved' });
+          }
+        });
+      }
+    }
+  );
+});
+
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/api/user/login');
+  });
+});
+
+export default router;
